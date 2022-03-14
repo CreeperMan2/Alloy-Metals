41f5b65bc516ff22b7069f6a81b3d2267d726135
CreeperMan2 <masterblaster10021@gmail.com>
2022-03-13 20:36:41 -0700

    e

---
+++ src/main/java/net/creeperman2/alloymetals/item/ModItems.java
@@ -0,0 +1,24 @@
+package net.creeperman2.alloymetals.item;
+
+import com.mojang.datafixers.TypeRewriteRule;
+import net.creeperman2.alloymetals.AlloyMetals;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+
+public class ModItems {
+    //items
+    public static final Item ZINC_INGOT = registerItem("zinc_ingot",
+            new Item(new FabricItemSettings().group(ItemGroup.MATERIALS)));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registry.ITEM, new Identifier(AlloyMetals.MOD_ID, name), item);
+    }
+    public static void registerModItems() {
+        AlloyMetals.LOGGER.info("Register Mod Items");
+    }
+
+}
