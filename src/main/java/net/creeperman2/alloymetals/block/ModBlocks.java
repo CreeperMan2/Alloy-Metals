28bb0beea799c66d13ebf48ca332e6dce93902a3
CreeperMan2 <masterblaster10021@gmail.com>
2022-03-14 18:37:08 -0700

    changed zinc block texture/item group

--- src/main/java/net/creeperman2/alloymetals/block/ModBlocks.java
+++ src/main/java/net/creeperman2/alloymetals/block/ModBlocks.java
@@ -14,7 +14,7 @@ import net.minecraft.util.registry.Registry;
 public class ModBlocks {
 
     public static final Block ZINC_BLOCK = registerBlock("zinc_block",
-            new Block(FabricBlockSettings.of(Material.METAL).strength(2f).requiresTool()), ItemGroup.MISC);
+            new Block(FabricBlockSettings.of(Material.METAL).strength(2f).requiresTool()), ItemGroup.BUILDING_BLOCKS);
 
     private static Block registerBlock(String name, Block block, ItemGroup group) {
         registerBlockItem(name, block, group);
